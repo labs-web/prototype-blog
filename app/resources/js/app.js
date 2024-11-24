68bb79c6bfde686c8860f917e801604fe2e1b713
ESSARRAJ <essarraj.fouad@gmail.com>
2024-11-24 21:03:20 +0100

    save

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+import 'bootstrap';
+
+// Import AdminLTE
+import "admin-lte/dist/js/adminlte";
+import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle';
+import 'admin-lte/dist/js/adminlte';
+
+
+// TODO charger depuis admin lte plugin
+// Import jQuery
+import 'https://code.jquery.com/jquery-3.6.0.min.js';
+
+// TODO charger depuis admin lte plugin
+import 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js';
+
--- app/routes/web.php
+++ app/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+ 
+Auth::routes();
+Route::get('/', [AdminController::class, 'index']);
