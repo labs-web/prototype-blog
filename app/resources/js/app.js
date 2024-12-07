539e8a6545b4331f28ab52368f345916c3672387
ESSARRAJ <essarraj.fouad@gmail.com>
2024-12-07 19:05:03 +0100

    save

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,14 +1,6 @@
 import 'bootstrap';
-
-// Import AdminLTE
 import "admin-lte/dist/js/adminlte";
 import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle';
 import 'admin-lte/dist/js/adminlte';
-
-
-// TODO charger depuis admin lte plugin
-// Import jQuery
 import 'admin-lte/plugins/jquery/jquery.min.js';
-
-// TODO charger depuis admin lte plugin
 import 'admin-lte/plugins/select2/js/select2.min.js';
