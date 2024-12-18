98636d392c710350c5143fe2db5b15c059fe5472
ESSARRAJ <essarraj.fouad@gmail.com>
2024-12-18 20:17:56 +0100

    add Permission package

---
+++ app/modules/PkgPermission/Database/Seeders/PkgPermissionSeeder.php
@@ -0,0 +1,15 @@
+<?php
+// Ce fichier est maintenu par ESSARRAJ Fouad
+
+
+
+namespace Modules\PkgPermission\Database\Seeders;
+use Modules\Core\Database\Seeders\CoreSeeder;
+
+class PkgPermissionSeeder extends CoreSeeder
+{
+    public function run(): void
+    {
+        parent::loadAndRun(__DIR__,__NAMESPACE__);
+    }
+}
---
+++ app/modules/PkgPermission/resources/views/layouts/_sidebar.blade.php
@@ -0,0 +1,15 @@
+{{-- Ce fichier est maintenu par ESSARRAJ Fouad --}}
+
+<li class="nav-item has-treeview {{ Request::is('PkgPermission*') ? 'menu-open' : '' }}">
+    <a href="#" class="nav-link nav-link {{ Request::is('PkgPermission*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-table"></i>
+        <p>
+            PkgPermission
+            <i class="fas fa-angle-left right"></i>
+        </p>
+    </a>
+    <ul class="nav nav-treeview">
+    </ul>
+</li>
+
+
